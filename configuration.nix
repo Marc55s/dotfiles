83f2b7b356fc43e2e7b10ef75b35cca555427e83
marc55s <marc.schoenig@gmail.com>
2025-02-08 19:17:34 +0100

    feat: greetd to tuigreet

--- configuration.nix
+++ configuration.nix
@@ -81,7 +81,7 @@
         enable = true;
         settings = {
             default_session = {
-                command = "${pkgs.greetd.greetd}/bin/agreety --cmd Hyprland";
+                command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd Hyprland";
             };
         };
 
