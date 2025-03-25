e8c6177ff6a86f2769beec5bba60cbe854fcb9da
marc55s <marc.schoenig@gmail.com>
2025-03-25 22:43:37 +0100

    refactor: neovim and lsps

--- home/home.nix
+++ home/home.nix
@@ -1,12 +1,6 @@
 {config, pkgs, pkgs-unstable, inputs,  ... }: {
 
     home.packages = with pkgs; [ 
-        # LSPs
-        texlab
-        lua-language-server
-        nil
-        pylyzer
-        pyright
 
         spotify
         vscode
@@ -41,8 +35,6 @@
         nautilus
         texliveFull
         imagemagick
-        tree-sitter-grammars.tree-sitter-latex
-        tree-sitter
         ghostscript
         whatsapp-for-linux
         pkgs-unstable.presenterm
@@ -59,6 +51,7 @@
         ./hypr/default.nix
         ./ctf.nix
         ./zathura.nix
+        ./nvim.nix
     ];
 
     home.shellAliases = {
@@ -88,28 +81,6 @@
 
     programs.ripgrep.enable = true;
 
-    programs.neovim =  {
-        enable = true;
-        defaultEditor = true;
-        vimAlias = true;
-        viAlias = true;
-        package = pkgs-unstable.neovim.unwrapped;
-        plugins = with pkgs.vimPlugins; [
-            # ... your other plugins
-            (nvim-treesitter.withPlugins (_: nvim-treesitter.allGrammars ++ [
-                (pkgs.tree-sitter.buildGrammar {
-                    language = "just";
-                    version = "8af0aab";
-                    src = pkgs.fetchFromGitHub {
-                        owner = "IndianBoy42";
-                        repo = "tree-sitter-just";
-                        rev = "8af0aab79854aaf25b620a52c39485849922f766";
-                        sha256 = "sha256-hYKFidN3LHJg2NLM1EiJFki+0nqi1URnoLLPknUbFJY=";
-                    };
-                })
-            ]))
-        ];
-    };
 
     programs.gh.enable = true;
 
---
+++ home/nvim.nix
@@ -0,0 +1,36 @@
+{pkgs, pkgs-unstable, ... }:
+{
+    home.packages = with pkgs;[
+        # LSPs
+        texlab
+        lua-language-server
+        nil
+        pylyzer
+        pyright
+        tree-sitter-grammars.tree-sitter-latex
+        tree-sitter
+    ];
+
+    programs.neovim =  {
+        enable = true;
+        defaultEditor = true;
+        vimAlias = true;
+        viAlias = true;
+        package = pkgs-unstable.neovim.unwrapped;
+        plugins = with pkgs.vimPlugins; [
+            # ... your other plugins
+            (nvim-treesitter.withPlugins (_: nvim-treesitter.allGrammars ++ [
+                (pkgs.tree-sitter.buildGrammar {
+                    language = "just";
+                    version = "8af0aab";
+                    src = pkgs.fetchFromGitHub {
+                        owner = "IndianBoy42";
+                        repo = "tree-sitter-just";
+                        rev = "8af0aab79854aaf25b620a52c39485849922f766";
+                        sha256 = "sha256-hYKFidN3LHJg2NLM1EiJFki+0nqi1URnoLLPknUbFJY=";
+                    };
+                })
+            ]))
+        ];
+    };
+}
