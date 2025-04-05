25a6b391e2295a844acececb5c97f0dc2ad1606c
marc55s <marc.schoenig@gmail.com>
2025-04-05 21:14:40 +0200

    zathura light mode

--- home/zathura.nix
+++ home/zathura.nix
@@ -3,13 +3,13 @@
     programs.zathura = {
         enable = true;
         options = {
-            default-bg = "#2A2A2A";          # Background color
-            statusbar-bg = "#3c3836";        # Status bar color
+            #default-bg = "#2A2A2A";          # Background color
+            #statusbar-bg = "#3c3836";        # Status bar color
             highlight-active = true;         # Highlight links
             selection-clipboard = "clipboard"; # Use system clipboard
             adjust-open = "best-fit";        # Adjust zoom level
             synctex = true;                  # Enable SyncTeX support
-            recolor = true;                   # Enable dark mode
+            #recolor = true;                   # Enable dark mode
         };
     };
 }
