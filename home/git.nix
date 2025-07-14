e54e49ac3a8f4dd1ebc7b758eec5733b381f2f31
marc55s <marc.schoenig@gmail.com>
2025-07-14 10:54:28 +0200

    feat: delta git diff

--- home/git.nix
+++ home/git.nix
@@ -3,6 +3,12 @@
         enable = true;
         userEmail = "marc.schoenig@gmail.com";
         userName = "marc55s";
+        delta = {
+            enable = true;
+            options = {
+                features = "decorations";
+            };
+        };
         extraConfig = {
             push.autoSetupRemote = true;
             rerere.enabled = true;
