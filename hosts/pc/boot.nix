c82c4970df65152e374345063b3b4801b72b07a0
marc55s <marc.schoenig@gmail.com>
2026-05-10 15:18:33 +0200

    refactor: remove zfs

--- hosts/pc/boot.nix
+++ hosts/pc/boot.nix
@@ -1,25 +1,14 @@
-{pkgs, config, ...}:
-{
+{ pkgs, config, ... }: {
   boot.initrd.systemd.enable = true;
-  boot.initrd.luks.devices."cryptroot".device =
-    "/dev/disk/by-uuid/b9fb3b65-bc59-48f7-8f31-94b5e60e0d16";
-  boot.initrd.luks.devices."cryptroot".preLVM = true;
-  networking.hostId = "c5a47126";
-  boot.zfs.devNodes = "/dev/mapper";
-  boot.zfs.extraPools = [ "zroot" ];
-  boot.zfs.requestEncryptionCredentials = true;
-
-    boot.kernelPackages = pkgs.linuxPackages_6_6;
+  boot.kernelPackages = pkgs.linuxPackages_6_6;
 
   # Force high performance and prevent "Scatter/Gather" memory bugs on Navi 10
   boot.kernelParams = [
     "amdgpu.ppfeaturemask=0xffffffff"
     "amdgpu.runpm=0" # DISABLES Runtime Power Management entirely
     "amdgpu.dcfeaturemask=0x2" # Forces the display core to prioritize refresh rate over power
-    "amdgpu.dcdebugmask=0x10"        # Disables PSR (Stops the FPS decay)
-    "zfs.zfs_arc_max=4294967296"
+    "amdgpu.dcdebugmask=0x10" # Disables PSR (Stops the FPS decay)
   ];
-
 
   # Stop the service that is fighting your manual echo commands
   systemd.services.power-profiles-daemon.enable = false;
@@ -31,8 +20,6 @@
     grub = {
       enable = true;
       efiSupport = true;
-      zfsSupport = true;
-      copyKernels = true;
       device = "nodev"; # required for UEFI systems
     };
   };
