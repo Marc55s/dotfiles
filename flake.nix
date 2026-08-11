cbfb50617de0522e977d1e2c181166f56903a26c
marc55s <marc.schoenig@gmail.com>
2026-08-11 14:28:46 +0200

    refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -1,172 +1,142 @@
 {
-    description = "A very basic flake";
+  description = "NixOS + home-manager configurations for pc, laptop, mainframe and monolith";
 
-    inputs = {
-        nixpkgs.url = "nixpkgs/nixos-26.05";
-        nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+  inputs = {
+    # --- nixpkgs channels -------------------------------------------------
+    nixpkgs.url = "nixpkgs/nixos-26.05";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    nixpkgs-openconnect-sso.url = "github:nixos/nixpkgs/46397778ef1f73414b03ed553a3368f0e7e33c2f";
 
-        nix-index-database = {
-            url = "github:nix-community/nix-index-database";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        noctalia = {
-          url = "github:noctalia-dev/noctalia-shell";
-          inputs.nixpkgs.follows = "nixpkgs";
-        };
-
-        home-manager = {
-            url = "github:nix-community/home-manager/release-26.05";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-
-        disko = {
-            url = "github:nix-community/disko";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-
-        grub2-themes = {
-            url = "github:vinceliuice/grub2-themes";
-        };
-
-        edu-sync-nix = {
-            url = "github:Marc55s/edu-sync-nix";
-            inputs.nixpkgs.follows = "nixpkgs-unstable";
-        };
-
-        wakafetch = {
-            url = "github:marc55s/wakafetch";
-            inputs.nixpkgs.follows = "nixpkgs-unstable";
-        };
-
-        openconnect-sso = {
-            url = "github:jcszymansk/openconnect-sso";
-            inputs.nixpkgs.follows = "nixpkgs-openconnect-sso";
-        };
-
-        nixpkgs-openconnect-sso.url = "github:nixos/nixpkgs/46397778ef1f73414b03ed553a3368f0e7e33c2f";
-        termstat = {
-            url = "github:marc55s/termstat";
-            inputs.nixpkgs.follows = "nixpkgs-unstable";
-        };
-
-        niri = {
-          url = "github:sodiboo/niri-flake";
-          inputs.nixpkgs.follows = "nixpkgs";
-        };
-
-        spicetify-nix = {
-          url = "github:Gerg-L/spicetify-nix";
-        };
+    # --- system tooling ---------------------------------------------------
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    outputs = inputs@{self, nixpkgs, nixpkgs-unstable, nix-index-database, home-manager, grub2-themes, ... }:
-        let
-            system = "x86_64-linux";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-            overlay = final: prev: {
-                edu-sync-cli = inputs.edu-sync-nix.packages.${pkgs.stdenv.hostPlatform.system}.default;
-                wakafetch = inputs.wakafetch.packages.${pkgs.stdenv.hostPlatform.system}.default;
-                openconnect-sso = inputs.openconnect-sso.packages.${pkgs.stdenv.hostPlatform.system}.openconnect-sso;
-                tailscale = prev.tailscale.overrideAttrs (oldAttrs: {
-                    # Remove the failing test file during build
-                    preBuild = (oldAttrs.preBuild or "") + ''
-                    rm -f portlist/portlist_test.go
-                    '';
-                });
-                spicePkgs = inputs.spicetify-nix.legacyPackages.${system};
-            };
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-            pkgs = import nixpkgs {
-                inherit system;
-                config = { allowUnfree = true; };
-                overlays = [ overlay inputs.termstat.overlays.default ];
-            };
+    grub2-themes.url = "github:vinceliuice/grub2-themes";
 
-            pkgs-unstable = import nixpkgs-unstable {
-                inherit system;
-                config = { allowUnfree = true; };
-                overlays = [ overlay ];
-            };
+    # --- desktop ----------------------------------------------------------
+    niri = {
+      url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-            lib = nixpkgs.lib;
-        in {
-            nixosConfigurations = {
-                pc = lib.nixosSystem {
-                    inherit pkgs;
-                    modules = [
-                        nix-index-database.nixosModules.default 
-                        ./hosts/pc/configuration.nix
-                        ./hosts/pc/hardware-configuration.nix
-                        home-manager.nixosModules.home-manager
-                        {
-                            home-manager.useGlobalPkgs = true;
-                            home-manager.useUserPackages = true;
-                            home-manager.users.marc = import ./home/pc.nix;
-                            home-manager.extraSpecialArgs = {
-                                inherit inputs pkgs-unstable;
-                                hostName = "pc";
-                            };
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-                        }
-                    ];
-                };
+    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
 
-                laptop = lib.nixosSystem {
-                    inherit pkgs;
-                    modules = [
-                        grub2-themes.nixosModules.default
-                        ./hosts/laptop/configuration.nix
-                        ./hosts/laptop/hardware-configuration.nix
-                        home-manager.nixosModules.home-manager
-                        {
-                            home-manager.useGlobalPkgs = true;
-                            home-manager.useUserPackages = true;
-                            home-manager.users.marc = import ./home/laptop.nix;
-                            home-manager.extraSpecialArgs = {
-                                inherit inputs pkgs-unstable;
-                                hostName = "laptop";
-                            };
-                        }
-                    ];
-                };
+    # --- custom packages -----------------------------------------------------
+    edu-sync-nix = {
+      url = "github:Marc55s/edu-sync-nix";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
 
-                mainframe = lib.nixosSystem {
-                    inherit pkgs;
-                    modules = [
-                        ./hosts/mainframe/configuration.nix
-                        ./hosts/mainframe/hardware-configuration.nix
-                        inputs.disko.nixosModules.disko
-                        home-manager.nixosModules.home-manager
-                        {
-                            home-manager.useGlobalPkgs = true;
-                            home-manager.useUserPackages = true;
-                            # home-manager.users.mainframe = import ./home/pc.nix;
-                            home-manager.extraSpecialArgs = {
-                                inherit inputs pkgs-unstable;
-                                hostName = "mainframe";
-                            };
+    wakafetch = {
+      url = "github:marc55s/wakafetch";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
 
-                        }
-                    ];
-                };
+    termstat = {
+      url = "github:marc55s/termstat";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
 
-                monolith = lib.nixosSystem {
-                    inherit pkgs;
-                    modules = [
-                        ./hosts/monolith/configuration.nix
-                        ./hosts/monolith/hardware-configuration.nix
-                        inputs.disko.nixosModules.disko
-                        home-manager.nixosModules.home-manager
-                        {
-                            home-manager.useGlobalPkgs = true;
-                            home-manager.useUserPackages = true;
-                            home-manager.extraSpecialArgs = {
-                                inherit inputs pkgs-unstable;
-                            };
+    openconnect-sso = {
+      url = "github:jcszymansk/openconnect-sso";
+      inputs.nixpkgs.follows = "nixpkgs-openconnect-sso";
+    };
+  };
 
-                        }
-                    ];
-                };
-            };
+  outputs =
+    inputs:
+    let
+      inherit (inputs) nixpkgs nixpkgs-unstable;
+      inherit (nixpkgs) lib;
+
+      system = "x86_64-linux";
+
+      overlay = final: prev: {
+        edu-sync-cli = inputs.edu-sync-nix.packages.${system}.default;
+        wakafetch = inputs.wakafetch.packages.${system}.default;
+        openconnect-sso = inputs.openconnect-sso.packages.${system}.openconnect-sso;
+        spicePkgs = inputs.spicetify-nix.legacyPackages.${system};
+
+        # portlist_test.go fails during the build; drop it beforehand.
+        tailscale = prev.tailscale.overrideAttrs (old: {
+          preBuild = (old.preBuild or "") + ''
+            rm -f portlist/portlist_test.go
+          '';
+        });
+      };
+
+      mkPkgs =
+        source: extraOverlays:
+        import source {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ overlay ] ++ extraOverlays;
         };
+
+      pkgs = mkPkgs nixpkgs [ inputs.termstat.overlays.default ];
+      pkgs-unstable = mkPkgs nixpkgs-unstable [ ];
+
+      hosts = {
+        pc = {
+          modules = [ inputs.nix-index-database.nixosModules.default ];
+          users.marc = ./home/pc.nix;
+        };
+
+        laptop = {
+          modules = [ inputs.grub2-themes.nixosModules.default ];
+          users.marc = ./home/laptop.nix;
+        };
+
+        mainframe.modules = [ inputs.disko.nixosModules.disko ];
+
+        monolith.modules = [ inputs.disko.nixosModules.disko ];
+      };
+
+      mkHost =
+        hostName:
+        {
+          modules ? [ ],
+          users ? { },
+        }:
+        lib.nixosSystem {
+          inherit pkgs;
+          specialArgs = { inherit inputs pkgs-unstable hostName; };
+          modules = [
+            ./hosts/${hostName}/configuration.nix
+            ./hosts/${hostName}/hardware-configuration.nix
+            inputs.home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                extraSpecialArgs = { inherit inputs pkgs-unstable hostName; };
+                inherit users;
+              };
+            }
+          ]
+          ++ modules;
+        };
+    in
+    {
+      nixosConfigurations = lib.mapAttrs mkHost hosts;
+
+      formatter.${system} = pkgs.nixfmt-rfc-style;
+    };
 }
