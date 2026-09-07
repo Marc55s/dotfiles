cfd21c15a40abb4fc21aa401b76652d1ba01826e
marc55s <marc.schoenig@gmail.com>
2026-09-07 14:42:31 +0200

    update keys after reinstall

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -1,6 +1,7 @@
 let
    admin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDmMEHJkey2YR4q7pgJVcgq8mi3Wfu2rJnwnQiMAoLjW marc@pc";
-   monolith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINd3fYoWvCv2hrkjtOj9uYZgb0l9y//1Vsa8LEd+8qbc";
+   monolith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDAG03ajGpK6Nln4fR9iVzftBlfyYAovyrxmEvCmazXW";
+
 in
 {
   "ionos-ddns-url.age".publicKeys = [
